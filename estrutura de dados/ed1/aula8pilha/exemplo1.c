bee6635483861694f28c33564fb440f90733f5b8
vitoriacalonga <133722930+vitoriacalonga@users.noreply.github.com>
2025-02-12 10:31:36 -0300

    Update exemplo1.c

--- estrutura de dados/ed1/aula8pilha/exemplo1.c
+++ estrutura de dados/ed1/aula8pilha/exemplo1.c
@@ -17,35 +17,86 @@ pilha - empilhar, desempilhar, topo, impressão(apenas para visualizacao)
 
 #define MAX 10
 
-// Definicao da estrutura fila
+/**
+ * @brief Estrutura para representar uma fila.
+ */
 struct est_fila{
-    int vet_fila[MAX];
-    int cont;
+    int vet_fila[MAX]; /**< Vetor que armazena os elementos da fila. */
+    int cont; /**< Contador que indica o número de elementos na fila. */
 };
 
 typedef struct est_fila tipo_fila;
 
-//definicao da estrutura pilha
+/**
+ * @brief Estrutura para representar uma pilha.
+ */
 struct est_pilha{
-    int vet_pilha[MAX];
-    int cont;
+    int vet_pilha[MAX]; /**< Vetor que armazena os elementos da pilha. */
+    int cont; /**< Contador que indica o número de elementos na pilha. */
 };
 
 typedef struct est_pilha tipo_pilha;
 
-// Protótipo das funcoes
-void insere_fila(tipo_fila *, int);
-int remove_fila (tipo_fila*);
-void imprime_fila(tipo_fila);
+/**
+ * @brief Insere um elemento na fila.
+ * 
+ * @param fl Ponteiro para a estrutura da fila.
+ * @param vl Valor a ser inserido na fila.
+ */
+void insere_fila(tipo_fila *fl, int vl);
 
-void empilha(tipo_pilha*, int);
-int desempilha(tipo_pilha*);
-int topo(tipo_pilha);
-void imprime_pilha(tipo_pilha);
+/**
+ * @brief Remove um elemento da fila.
+ * 
+ * @param fl Ponteiro para a estrutura da fila.
+ * @return int Valor removido da fila.
+ */
+int remove_fila (tipo_fila* fl);
 
-// Função principal
+/**
+ * @brief Imprime todos os elementos da fila.
+ * 
+ * @param fl Estrutura da fila.
+ */
+void imprime_fila(tipo_fila fl);
 
-int main(/*int argc, char *argv[]*/){//procurar sobre os arg do parametro
+/**
+ * @brief Empilha um elemento na pilha.
+ * 
+ * @param pl Ponteiro para a estrutura da pilha.
+ * @param vl Valor a ser empilhado.
+ */
+void empilha(tipo_pilha* pl, int vl);
+
+/**
+ * @brief Desempilha um elemento da pilha.
+ * 
+ * @param pl Ponteiro para a estrutura da pilha.
+ * @return int Valor desempilhado.
+ */
+int desempilha(tipo_pilha* pl);
+
+/**
+ * @brief Retorna o elemento do topo da pilha.
+ * 
+ * @param pl Estrutura da pilha.
+ * @return int Valor no topo da pilha.
+ */
+int topo(tipo_pilha pl);
+
+/**
+ * @brief Imprime todos os elementos da pilha.
+ * 
+ * @param pl Estrutura da pilha.
+ */
+void imprime_pilha(tipo_pilha pl);
+
+/**
+ * @brief Função principal.
+ * 
+ * @return int 
+ */
+int main(){
     tipo_fila minha_fila;
     tipo_pilha minha_pilha;
     minha_fila.cont = 0;
@@ -56,7 +107,6 @@ int main(/*int argc, char *argv[]*/){//procurar sobre os arg do parametro
         scanf("%d", &valor);
         insere_fila(&minha_fila, valor);
     }
-
 
     empilha(&minha_pilha, 11); 
     empilha(&minha_pilha, 22);
@@ -72,33 +122,28 @@ int main(/*int argc, char *argv[]*/){//procurar sobre os arg do parametro
     return 0;
 }
 
-//FUNCAO INSERCAOO NA FILA
 void insere_fila(tipo_fila *fl, int vl){
-    //verifica se a fila esta cheia
-    if(fl->cont < MAX){//entender o ->
+    // Verifica se a fila está cheia
+    if(fl->cont < MAX){
         fl->vet_fila[fl->cont++] = vl;
     }else{
         printf("[ATENCAO] Estrutura FILA esta cheia! Valor nao foi inserido.\n");
     }
 }
 
-//funcao que empilha
 void empilha(tipo_pilha *pl, int vl){
     if(pl->cont < MAX){
         pl->vet_pilha[pl->cont] = vl;
         pl->cont++;
-    }
-    else{
+    }else{
         printf("[ATENCAO] Estrutura PILHA esta cheia! Valor nao foi inserido.\n");
     }
 }
 
-//impreme a fila
 void imprime_fila(tipo_fila fl){
     if(fl.cont == 0){
-        printf("Fila vazia/n");
-    }
-    else{
+        printf("Fila vazia\n");
+    }else{
         printf("FILA: [");
         for(int i = 0; i < fl.cont; i++){
             printf("%d ", fl.vet_fila[i]);
@@ -107,58 +152,41 @@ void imprime_fila(tipo_fila fl){
     }
 }
 
-//funcao imprime pilha
-
-
-/*---------------------------------
---diferença entre o . para acessar memoria e a ->:-
-o ponto usamos quando queremos acesar diretamente aql variavel sem interferir, 
-já a setinha usamos no lugar do * quando estamos manipulando ponteiro
----------------------------------*/
-
-//funcao topo
 int topo(tipo_pilha pl){
     int aux;
-
-    //verifica se a pilha tem valor
+    // Verifica se a pilha tem valor
     if(pl.cont > 0){
         aux = pl.vet_pilha[pl.cont];
-    }
-    else{
+    }else{
         printf("[ATENCAO] Pilha esta vazia. Nao ha valor a ser retornado.\n");
     }
     return aux;
 }
 
-//funcao desempilha
 int desempilha(tipo_pilha *pl){
     int aux;
     if(pl->cont > 0){
         aux = pl->vet_pilha[pl->cont];
-        for(int i = pl->cont; pl->cont-1;i++){
+        for(int i = pl->cont; pl->cont-1; i++){
             pl->vet_pilha[i] = 0;
         }
         pl->cont--;
-    }
-    else{
+    }else{
         printf("[ATENCAO] Pilha esta vazia. Nao ha valor a ser retornado.\n");
     }
     return aux;
 }
 
-
-//remove fila
 int remove_fila(tipo_fila *fl){
     int aux;
-    //verifica se fila tem valor
+    // Verifica se a fila tem valor
     if(fl->cont > 0){
-        aux = fl->vet_fila[0]; //armazena o primeiro valor da fila, para retorna-lo
-        for(int i = 0; fl->cont-1;i++){
+        aux = fl->vet_fila[0]; // Armazena o primeiro valor da fila, para retorná-lo
+        for(int i = 0; fl->cont-1; i++){
             fl->vet_fila[i]=fl->vet_fila[i+1];
         }
         fl->cont--;
-    }
-    else{
+    }else{
         printf("[ATENCAO] Fila esta vazia. Nao ha valor a ser retornado.\n");
     }
     return aux;
